fc652615817afad87c48899e61df5649119db3bf
tff-wvw <shprykovad@mail.ru>
2024-12-19 10:13:03 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Diagnostics.Eventing.Reader;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string userLogin = "Aboba";
+            string userPassword = "Lashped";
+
+            if (string.IsNullOrEmpty(TbLogin.Text) && string.IsNullOrEmpty(PbPSW.Password))
+            {
+                MessageBox.Show("Net!");
+
+            }
+            else
+            {
+                if (TbLogin.Text == userLogin && PbPSW.Password == userPassword)
+                {
+                    MessageBox.Show("Povezlo!");
+                }
+                else
+                {
+                    MessageBox.Show("Net2");
+                }
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
+
